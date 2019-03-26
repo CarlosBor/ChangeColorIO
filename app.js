013cda14b299977be984919e4cb2a83a571c1380
CarlosBor <carlosborjawebdev@gmail.com>
2019-03-26 22:47:20 +0100

    Initial working version

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = require('http').createServer(response);
+var fs = require('fs');
+var io = require('socket.io')(app);
+
+app.listen(3000);
+console.log("App running…");
+
+function response(req, res) {
+    var file = "";
+    if (req.url == "/") {
+        file = __dirname + '/index.html';
+    } else {
+        file = __dirname + req.url;
+    }
+    fs.readFile(file, function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            return res.end('Page or file not found');
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+io.on("connection", function(socket) {
+    socket.on("colorSwap", function(color, callback) {
+        color = "red";
+        io.sockets.emit("executeChange", color);
+        callback();
+    });
+});
